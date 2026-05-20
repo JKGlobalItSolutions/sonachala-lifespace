6824185e9cb5ad7e9fafb6cc958f251426096251
sudhar-dev-02 <sudharsanp300@gmail.com>
2026-05-20 13:58:58 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/",
+  base: "/sonachala-lifespace/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
